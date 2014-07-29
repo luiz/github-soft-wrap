a2f0e710682d03ea72d3860965668336cc2414d8
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2014-07-29 16:32:27 -0300

    Reindenting with tabs

--- github-soft-wrap.user.js
+++ github-soft-wrap.user.js
@@ -7,42 +7,42 @@
 // @grant       none
 // ==/UserScript==
 (function($) {
-  function toggleOnDiff(container) {
-    container.toggleClass("soft-wrap");
-  }
-  function toggleOnFile(container) {
-    var $pre = container.find("pre"),
-        $lines = $pre.find(".line");
-    if ($pre.css("white-space") == "pre") {
-      $pre.css("white-space", "pre-wrap");
-      $lines.css("height", "auto");
-    } else {
-      $pre.css("white-space", "pre");
-    }
-    $lines.each(function() {
-      var $line = $(this),
-          correspondingSpanId = "#" + $line.attr("id").replace("LC", "L");
-      $(correspondingSpanId).css("height", $line.css("height"));
-    });
-  }
-  $("<button>")
-    .addClass("minibutton", "soft-wrap")
-    .text("Soft wrap")
-    .prependTo(".file:has(.file-code) .actions")
-    .click(function() {
-      var $this = $(this),
-          $fileContainer = $this.closest(".file");
-      if ($fileContainer.has("pre").length == 0) {
-        toggleOnDiff($fileContainer);
-      } else {
-        toggleOnFile($fileContainer);
-      }
-      $this.toggleClass("selected");
-    })
-    .each(function() {
-      var $this = $(this);
-      if ($this.closest(".file").is(".soft-wrap")) {
-        $this.addClass("selected");
-      }
-    });
+	function toggleOnDiff(container) {
+		container.toggleClass("soft-wrap");
+	}
+	function toggleOnFile(container) {
+		var $pre = container.find("pre"),
+		$lines = $pre.find(".line");
+		if ($pre.css("white-space") == "pre") {
+			$pre.css("white-space", "pre-wrap");
+			$lines.css("height", "auto");
+		} else {
+			$pre.css("white-space", "pre");
+		}
+		$lines.each(function() {
+			var $line = $(this),
+			correspondingSpanId = "#" + $line.attr("id").replace("LC", "L");
+			$(correspondingSpanId).css("height", $line.css("height"));
+		});
+	}
+	$("<button>")
+	.addClass("minibutton", "soft-wrap")
+	.text("Soft wrap")
+	.prependTo(".file:has(.file-code) .actions")
+	.click(function() {
+		var $this = $(this),
+		$fileContainer = $this.closest(".file");
+		if ($fileContainer.has("pre").length == 0) {
+			toggleOnDiff($fileContainer);
+		} else {
+			toggleOnFile($fileContainer);
+		}
+		$this.toggleClass("selected");
+	})
+	.each(function() {
+		var $this = $(this);
+		if ($this.closest(".file").is(".soft-wrap")) {
+			$this.addClass("selected");
+		}
+	});
 }(jQuery));
