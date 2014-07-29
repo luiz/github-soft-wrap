ad05fa991c4e5160f165a795e590b0077b4bb46b
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2014-07-29 12:37:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Github soft-wrap
+================
+
+This is a [Greasemonkey](https://addons.mozilla.org/pt-br/firefox/addon/greasemonkey/) script that adds a "Soft wrap" button for every file and diff shown on page. This way, you can toggle soft-wrap for any type of file, either disabling soft-wrap for prose diffs (which are [enabled by default](https://github.com/blog/1707-soft-wrapping-on-prose-diffs)) or enabling them for any other diff or file.
---
+++ github-soft-wrap.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name        Github soft-wrap toggler
+// @namespace   http://luiz.github.io
+// @description Adds a button "Soft-wrap" that toggles soft wrap on files and diffs shown on Github
+// @include     https://github.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+(function($) {
+  function toggleOnDiff(container) {
+    container.toggleClass("soft-wrap");
+  }
+  function toggleOnFile(container) {
+    var $pre = container.find("pre"),
+        $lines = $pre.find(".line");
+    if ($pre.css("white-space") == "pre") {
+      $pre.css("white-space", "pre-wrap");
+      $lines.css("height", "auto");
+    } else {
+      $pre.css("white-space", "pre");
+    }
+    $lines.each(function() {
+      var $line = $(this),
+          correspondingSpanId = "#" + $line.attr("id").replace("LC", "L");
+      $(correspondingSpanId).css("height", $line.css("height"));
+    });
+  }
+  $("<button>")
+    .addClass("minibutton", "soft-wrap")
+    .text("Soft wrap")
+    .prependTo(".file:has(.file-code) .actions")
+    .click(function() {
+      var $this = $(this),
+          $fileContainer = $this.closest(".file");
+      if ($fileContainer.has("pre").length == 0) {
+        toggleOnDiff($fileContainer);
+      } else {
+        toggleOnFile($fileContainer);
+      }
+      $this.toggleClass("selected");
+    })
+    .each(function() {
+      var $this = $(this);
+      if ($this.closest(".file").is(".soft-wrap")) {
+        $this.addClass("selected");
+      }
+    });
+}(jQuery));
